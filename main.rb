856e7061081adba9ad3ff9ca3ee85cd0750ab89b
shunsuke.taga <tagashunsuke@icloud.com>
2023-01-07 08:18:49 +0000

    second push

--- main.rb
+++ main.rb
@@ -8,10 +8,10 @@ sato = Human.new('佐藤 花子', 20, '映画')
 #メソッドを実行
 tanaka.say
 tanaka.think(tanaka.hoby)
-tanaka.think2
+# tanaka.think2
 suzuki.say
 suzuki.think(suzuki.hoby)
-suzuki.think2
+# suzuki.think2
 sato.say
 sato.think(sato.hoby)
 sato.think2
